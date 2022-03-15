d54d8011303a1cd9ec7e3ac7a511ec3b3a16a43a
Rabiul hossain <mohammadrabiul4@gmail.com>
2022-03-15 05:14:25 -0700

    subsequence problem.

--- SubSeq.js
+++ SubSeq.js
@@ -10,4 +10,22 @@ var subseq = function(p, up){
     subseq(p , up.substring(1, up.length));
 }
 
+
+var subseqRet = function(p, up){
+    var list = [];
+
+    if(up == ""){
+        list.push(p);
+        return list;
+    }
+
+    var ch = up.charAt(0);
+
+    var left = subseqRet(p + ch, up.substring(1, up.length));
+    var right = subseqRet(p, up.substring(1, up.length));
+    list = left.concat(right);
+    return list;
+}
+
 console.log(subseq("", "abc"));
+console.log(subseqRet("", "abc"));
